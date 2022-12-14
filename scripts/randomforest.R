dd3b60e4b97980fe7055a7097532abc8ec533eda
Javi <javiergalandiaz@gmail.com>
2022-12-14 14:42:42 +0100

    update randomforest

--- scripts/randomforest.R
+++ scripts/randomforest.R
@@ -1,98 +1,78 @@
 
 library(tidyverse)
-# install.packages('randomForest')
 library(randomForest)
-# install.packages("caTools")
 library(caTools)
-# install.packages('party')
 library(party)
 
-# source("scripts/merge_all_data.R")
-
-#  EXPLORATORY DATA ANALYSIS #### 
-data <- read.csv("results/data_analyses.txt", sep="")
+# exploratory
+data <- read.csv("results/trait_final.txt", sep="")
 str(data)
 
-# clasifico las variables correctamente
+# format variables
+data$family <- as.factor(data$family)
+data$growth_form <- as.factor(data$growth_form)
+data$lifeform <- as.factor(data$lifeform)
+data$origin <- as.factor(data$origin)
+data$invasiveness <- as.factor(data$invasiveness)
 data$clonality <- as.factor(data$clonality)
 data$archaeophyte <- as.factor(data$archaeophyte)
+data$self.compatible <- as.factor(data$self.compatible)
 data$pollination <- as.factor(data$pollination)
 data$agochory <- as.factor(data$agochory)
 data$autochory <- as.factor(data$autochory)
 data$anemochory <- as.factor(data$anemochory)
 data$hydrochory <- as.factor(data$hydrochory)
 data$zoochory <- as.factor(data$zoochory)
-data$onset_flowering <- data$onset_flowering %>% round() %>% as.integer()
-data$length_bloom <- data$length_bloom %>% round() %>% as.integer()
-data$family <- as.factor(data$family)
-data$growth_form <- as.factor(data$growth_form)
-data$lifeform <- as.factor(data$lifeform)
-data$origin <- as.factor(data$origin)
-data$invasiveness <- as.factor(data$invasiveness)
 
 summary(data)
 
+# remove variables with too many NAs
+data <- data %>% dplyr::select(-c('RD','SRA','self.compatible'))
 
-pairs(data[,c("SLA","LDMC","SRL","RDMC",           
-              "dC13","LNC","height","CN",         
-              "seed_weight","onset_flowering","length_bloom")])
-
-data$CN <- NULL # es lo mismo que LNC
-
-# correlacion entre variables macro
-pairs(data[,c("bio1.sd","bio7.m","bio12.sd",       
-              "bio15.m","AOO","yrange")])
-
-# boxplots por origen
-vtraits <- colnames(data)[2:10]
-par(mfrow=c(1,1))
-for (tr in vtraits) {
-  pl <- data %>% select(tr, origin); colnames(pl) <- c("trait","invasiveness")
-  pl <- na.omit(pl)
-  print(tr)
-  print(table(pl$trait, pl$invasiveness))
-}
-
-vtraits <- colnames(data)[27:32]
-par(mfrow=c(2,3))
-for (tr in vtraits) {
-  pl <- data %>% select(tr, origin); colnames(pl) <- c("trait","invasiveness")
-  pl <- na.omit(pl)
-  boxplot(pl$trait ~ pl$invasiveness, main=tr)
-}
-
-
-# RANDOM FOREST #### 
-
+# random forest
 # https://towardsdatascience.com/random-forest-in-r-f66adf80ec9
-varout <- c('species','invasiveness')
-data.mod <- data %>% select(-varout)
+varout <- c('species','origin','invasiveness')
+mypred <- setdiff(colnames(data), varout)
 
-# imputacion NAs en clonality y pollination
-ss_y <- data.mod$origin
-ss_x <- data.mod
-ss_x$origin <- NULL
-data.imp <- rfImpute(x=ss_x, y=ss_y, data=data.mod, iter=5)
-colnames(data.imp)[colnames(data.imp)=="ss_y"] <- "origin"
+# imputacion NAs
+data.imp <- rfImpute(family~., data=data[,mypred], iter=5)
+data.imp$origin <- data$origin
+data.imp$invasiveness <- data$invasiveness
 
-# get best ntry
+# best ntry = 5
 par(mfrow=c(1,1))
-tuneRF(y=data.imp[,1], x=data.imp[,-1], improve=0.05, ntreeTry=50)
+tuneRF(y=data$origin, x=data.imp, improve=0.0005, ntreeTry=300)
+sqrt(ncol(data.imp))
 
-sample = sample.split(data.imp$origin, SplitRatio = .75)
+sample = sample.split(data.imp$origin, SplitRatio = .80)
 train = subset(data.imp, sample == TRUE)
 test  = subset(data.imp, sample == FALSE)
 
-rf1 <- randomForest(origin~., data=train, ntree=501, mtry=5, replace=T, importance=T)
+
+# predict origin
+rf1 <- randomForest(origin~., data=train[,-which(colnames(train)=='invasiveness')],
+                    ntree=500, mtry=5, replace=T, importance=T)
 rf1
 plot(rf1)
 rf1$importance
 
 varImpPlot(rf1)
 
-pred = predict(rf1, newdata=test[-1]) # cross-validation
-(cm = table(test[,1], pred))
+pred = predict(rf1, newdata=test[-which(colnames(train)=='invasiveness')]) # cross-validation
+(cm = table(test[,'origin'], pred))
 (sum(diag(cm))/sum(cm))
 
 
+# predict invasiveness
+rf1 <- randomForest(invasiveness~., data=train[,-which(colnames(train)=='origin')],
+                    ntree=500, mtry=5, replace=T, importance=T)
+rf1
+plot(rf1)
+rf1$importance
+
+varImpPlot(rf1)
+
+pred = predict(rf1, newdata=test[-which(colnames(train)=='origin')]) # cross-validation
+(cm = table(test[,'invasiveness'], pred))
+(sum(diag(cm))/sum(cm))
 
